f7346d4448636268a028312fd9132eff212a00d9
Jamz96 <69062399+Jamz96@users.noreply.github.com>
2025-02-04 18:51:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const respuestasCorrectas = {
     1: "23302571y", // Cambia esto por la respuesta correcta
     2: "648947202", // Cambia esto por la respuesta correcta
-    3: "12 de marzo" // Cambia esto por la respuesta correcta
+    3: "12/03", // Cambia esto por la respuesta correcta
+    4: "tenerife", // Cambia esto por la respuesta correcta
+    5: "melendi" // Cambia esto por la respuesta correcta
 };
 
 function verificarRespuesta(numeroPrueba) {
